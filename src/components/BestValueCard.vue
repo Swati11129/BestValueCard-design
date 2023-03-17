73af7182417189e7ebcfe8971d459243ce994990
Swati <swati11129@gmail.com>
2023-03-18 00:06:53 +0530

    ribbon updated

--- src/components/BestValueCard.vue
+++ src/components/BestValueCard.vue
@@ -2,7 +2,8 @@
 <v-app style="background-color:#E8E8E8;">
     <v-container >
             <v-card :elevation="2" class="card-position position rounded-xl " >
-                <div class="ribbon ribbon-top-left"><span>BEST VALUE</span></div>
+                <!-- <div class="ribbon ribbon-top-left"><span>BEST VALUE</span></div> -->
+                <div class="stack-top left" style="--d:12px;--w:200px; --c:skyblue;">1Month</div>
             </v-card>
     </v-container>
 </v-app>
@@ -35,33 +36,34 @@ export default {
     width: 100%;
     background-color:#FEFEFE;
 }
-.ribbon {
-    width: 150px;
-    height: 150px;
-    overflow: hidden;
-    position: absolute;
-}
-.ribbon span {
-    position: absolute;
-    display: block;
-    width: 225px;
-    padding: 15px 0;
-    background-color: #6FC2EE;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, .1);
-    color: #fff;
-    font: 700 18px/1 'Lato', sans-serif;
-    text-shadow: 0 1px 1px rgba(111, 194, 238, .2);
-    text-transform: uppercase;
-    text-align: center;
+
+.stack-top {
+  /* adjust the below to control the shape */
+  --d:5px; 
+  --w:100px;
+  --c:skyblue;
+  /**/
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(29.29%, -100%) rotate(45deg); /* 29.29% = 100%*(1 - cos(45deg)) */
+  color: #fff;
+  text-align: center;
+  width: var(--w);
+  transform-origin: bottom left;
+  padding:5px 0 calc(var(--d) + 5px);
+  background:
+    linear-gradient(rgba(0,0,0,0.6) 0 0) bottom/100% var(--d) no-repeat
+    var(--c);
+  clip-path:polygon(0 100%,0 calc(100% - var(--d)),50% calc(100% - var(--d) - var(--w)/2),100% calc(100% - var(--d)),100% 100%,calc(100% - var(--d)) calc(100% - var(--d)), var(--d) calc(100% - var(--d)))
 }
 
-.ribbon-top-left {
-    top: -10px;
-    left: -10px;
+.stack-top.left {
+  left:0;
+  right:auto;
+  transform: translate(-29.29%, -100%) rotate(-45deg);
+  transform-origin: bottom right;
 }
-.ribbon-top-left span {
-    right: -25px;
-    top: 30px;
-    transform: rotate(-45deg);
-}
+
 </style>
